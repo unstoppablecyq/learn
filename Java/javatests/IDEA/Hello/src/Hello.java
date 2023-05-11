7e7d5d9189ff2aa0eeaf60a40930bf26ba37adcd
unstoppablecyq <Lueecychuyiqing@163.com>
2023-05-11 22:46:18 +0800

    20230511-update

---
+++ Java/javatests/IDEA/Hello/src/Hello.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("//");
+
+    }
+
+}
+
+
+
+
+
+
+
+//
+//class test {
+//    public void bubble(int[] arr) {
+//        print("66");
+//    }
+//}
+
+//class Person{
+//    String name;
+//    int age;
+//
+//}
