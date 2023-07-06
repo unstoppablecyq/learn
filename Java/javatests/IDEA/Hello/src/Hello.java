9d8b14e123dd4cacda730696d65ba8affdcfddb4
unstoppablecyq <Lueecychuyiqing@163.com>
2023-07-06 12:09:29 +0800

    update_local_doc
    These days i'm learning and at the same time i'm in the company and do some jobs. So update what i've learned in recent ten days. :)

--- Java/javatests/IDEA/Hello/src/Hello.java
+++ Java/javatests/IDEA/Hello/src/Hello.java
@@ -2,16 +2,63 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("//");
+//        System.out.println("//");
+        int i=0;
+        Integer j = new Integer(0);
+        System.out.println(i==j);
+        System.out.println(j.equals(i));
 
+
+
+
+//        int s = 3784;
+//        int b = s;
+//        int a[] = {1,4,5,9,10,40,50,90,100,400,500,900,1000};
+//        String c[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+////        System.out.println(c[2]);
+////        System.out.println(a[2]);
+//        for(int i = 12; i>=0; i--){
+//            if (a[i] <= b){
+//                System.out.print(c[i]);
+//            }
+//        }
+//        int x = 121;
+//        if(x == 0){
+//            System.out.println("true");
+//        }
+//        if(x<0||x%10 == 0){
+//            System.out.println("false");
+//        }
+//        // int right = 0;
+//        // int left = 0;
+//        int a = x;
+//        int lo = 0;
+//        while(a!=0){
+//            a/=10;
+//            lo+=1;
+//        }
+//        // a = x;
+//        String numstr = Integer.toString(x);
+//        for(int i = 1; i<=lo/2; i++){
+//            //right = a%10;
+//            //left = Character.getNumericValue(numstr.charAt(i-1));
+//            // if(right != left){
+//            //     return false;
+//            // }
+//            if(x%10 != Character.getNumericValue(numstr.charAt(i-1))){
+//                System.out.println("false");
+//            }
+//            x/=10;
+//        }
+//        System.out.println("true");
     }
 
 }
 
 
-
-
-
+//
+//System.out.println("false");
+//System.out.println("true");
 
 
 //
---
+++ Java/javatests/IDEA/Hello/src/com/cccccyq/test001.java
@@ -0,0 +1,8 @@
+package com.cccccyq;
+import java.util.Scanner;
+public class test001 {
+    public static void main(String[] args){
+        System.out.print("//");
+    }
+}
+//test
